7e1841663fe81555d05d42ea4723c7e4dca5aa93
Yogesh Chiluka <yogeshravichiluka@gmail.com>
2024-06-04 19:54:38 +0530

    Add/update comment controllers and router

---
+++ src/controllers/comment.controllers.js
@@ -0,0 +1,111 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import {Comment} from "../models/comment.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import mongoose from "mongoose"
+
+
+
+const getVideoComments= asyncHandler(async(req,res) => {
+
+    const {videoId} = req.params
+    const {page = 1, limit = 10} = req.query
+
+    const  skip = (page - 1)*limit;
+
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from:"videos",
+                localField: "content",
+                foreignField: "_id",
+                as: "comment"
+            }
+        }
+    ]).skip(skip).limit(limit)
+
+    return res.status(200).json(
+        new ApiResponse(200, comments, "comment successfully fetched")
+    )
+
+})
+
+const addComment = asyncHandler(async(req,res) => {
+    
+    const {videoId} = req.params
+    const { comment } = req.body
+
+    const owner = req.user._id
+
+    if(!comment){
+        throw new ApiError(400, "Comment is required")
+    }
+
+    const newComment = await Comment.create(
+        {
+            content: comment,
+            videoId,
+            owner,
+        }
+    )
+
+    await newComment.save()
+
+    return res.status(200).json(
+
+        new ApiResponse(200, newComment, "Comment successfully added")
+    )
+})
+
+const updateComment = asyncHandler(async(req,res) => {
+
+    const {commentId} = req.params
+    const { comment } = req.body
+
+    if(!comment){
+        throw new ApiError(400, "Comment is required")
+    } 
+
+    const updateComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set : {
+                content: comment
+            }
+        },
+        {new: true}
+    )
+    await updateComment.save()
+
+    return res.status(200).json(
+        200,
+        new ApiResponse(200, updateComment, "Comment successfully updated")
+    )
+})
+
+const deleteComment = asyncHandler(async(req,res) => {
+
+    const {commentId} = req.params
+
+    await Comment.deleteOne({ _id:commentId })
+
+
+    return res.status(200).json(
+        200,
+        new ApiResponse(200, req.user, "Comment successfully deleted")
+    )
+})
+
+export {
+    getVideoComments,
+    addComment,
+    updateComment,
+    deleteComment 
+ }
+
+
